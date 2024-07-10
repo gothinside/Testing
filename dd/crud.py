0e07827d23036d12fbcb612e6701a8ae1aa36a3e
Ubuntu <>
2024-07-10 21:50:29 +0300

    dqdq

--- dd/crud.py
+++ dd/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-import models, schemas 
+from . import models, schemas
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
