eee4c76b460adae29ff763aa9ccd004e3c187fd5
gothinside <sveeeeer@mail.ru>
2024-07-10 23:49:34 +0300

    Update crud.py

--- dd/categories/crud.py
+++ dd/categories/crud.py
@@ -1,36 +1,60 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from fastapi import HTTPException, status
 from .. import models, schemas
-from fastapi import HTTPException
 
-def get_categories(db: Session, skip: int = 0, limit:int = 100):
+def get_categories(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Category).offset(skip).limit(limit).all()
 
 def create_category(db: Session, category: schemas.CategoryCreate):
-    category = models.Category(**category.model_dump())
-    db.add(category)
-    db.commit()
-    db.refresh(category)
-    return category
+    if db.query(models.Category).filter(models.Category.category_name == category.category_name).one_or_none():
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Category name must be unique")
 
-def update_category(db: Session, category_id:int, updated_category: schemas.CategoryUpdate):
+    new_category = models.Category(**category.model_dump())
+    
+    try:
+        db.add(new_category)
+        db.commit()
+        db.refresh(new_category)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+    
+    return new_category
+
+def update_category(db: Session, category_id: int, updated_category: schemas.CategoryUpdate):
     category = db.query(models.Category).filter(models.Category.id == category_id).one_or_none()
     
     if not category:
-        raise HTTPException(404, "Category not found") 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+    
+    if db.query(models.Category).filter(models.Category.category_name == updated_category.category_name).one_or_none():
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Category name must be unique")
 
     update_data = updated_category.model_dump(exclude_unset=True)
     for key, value in update_data.items():
         setattr(category, key, value)
     
-    db.commit()
-    db.refresh(category)
+    try:
+        db.commit()
+        db.refresh(category)
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+    
     return category
 
-def delete_category(db:Session, category_id:int):
+def delete_category(db: Session, category_id: int):
     category = db.query(models.Category).filter(models.Category.id == category_id).one_or_none()
     
     if not category:
-        raise HTTPException(404, "Category not found") 
-    db.delete(category)
-    db.commit()
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+    
+    try:
+        db.delete(category)
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+    
     return {"message": "Category deleted successfully"}
