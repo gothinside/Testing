5b268e8fca83740fd9a101250f73b28ad31b1880
gothinside <sveeeeer@mail.ru>
2024-07-10 21:45:48 +0300

    Update routers.py

--- dd/rooms/routers.py
+++ dd/rooms/routers.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Depends
-from ..crud import get_services, create_service
-from . import crud
-from ..database import SessionLocal, engine
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from .. import crud, schemas
+from ..database import SessionLocal, engine
 from ..models import Base
-from ..schemas import  RoomCreate, RoomUpdate
+
 def get_db():
     db = SessionLocal()
     try:
@@ -14,8 +13,23 @@ def get_db():
 
 router = APIRouter(
     prefix="/rooms",
-    tags=["rooms", "categories"]
+    tags=["rooms"]
 )
 
 Base.metadata.create_all(engine)
-session = SessionLocal()
+
+@router.get("/")
+async def read_rooms(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_rooms(db, skip, limit)
+
+@router.post("/")
+async def create_room(room: schemas.RoomCreate, db: Session = Depends(get_db)):
+    return crud.create_room(db, room)
+
+@router.patch("/{room_num}")
+async def update_room(room_num: int, updated_room: schemas.RoomUpdate, db: Session = Depends(get_db)):
+    return crud.update_room(db, room_num, updated_room)
+
+@router.delete("/{room_num}")
+async def delete_room(room_num: int, db: Session = Depends(get_db)):
+    return crud.delete_room(db, room_num)
